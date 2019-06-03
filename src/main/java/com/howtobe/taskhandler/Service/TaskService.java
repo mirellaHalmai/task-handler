b0f39f231668c7a6dabcf6acedab73d262ba01e0
mirellaHalmai <halmaimirella@gmail.com>
2019-06-03 15:19:01 +0200

    Base of task service

---
+++ src/main/java/com/howtobe/taskhandler/Service/TaskService.java
@@ -0,0 +1,20 @@
+package com.howtobe.taskhandler.Service;
+
+import com.howtobe.taskhandler.model.Task;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    public void addTask() {
+
+    }
+
+    public void deleteTask(Task task) {
+
+    }
+
+    public void updateTask(Task task) {
+
+    }
+}
