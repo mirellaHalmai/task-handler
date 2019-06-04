eef0041231219c21268b4c3b5e4bfb398f9902e4
mirellaHalmai <halmaimirella@gmail.com>
2019-06-04 15:35:14 +0200

    Adding simple http request logging

---
+++ src/main/java/com/howtobe/taskhandler/configuration/ServerLoggingConfig.java
@@ -0,0 +1,19 @@
+package com.howtobe.taskhandler.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class ServerLoggingConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter commonsRequestLoggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        return filter;
+    }
+}
