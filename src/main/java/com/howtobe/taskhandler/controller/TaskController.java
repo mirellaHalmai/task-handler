f30c3b1f107958e0678cd992faa403f81121713d
mirellaHalmai <halmaimirella@gmail.com>
2019-06-16 23:51:25 +0200

    checking request/response logging

--- src/main/java/com/howtobe/taskhandler/controller/TaskController.java
+++ src/main/java/com/howtobe/taskhandler/controller/TaskController.java
@@ -1,12 +1,25 @@
 package com.howtobe.taskhandler.controller;
 
+import com.howtobe.taskhandler.model.BudgetEntry;
 import com.howtobe.taskhandler.model.SimpleTask;
 import com.howtobe.taskhandler.model.Task;
 import com.howtobe.taskhandler.model.TaskType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
+
 @RestController
 public class TaskController {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @RequestMapping("/tasks")
     public Task getTasks(){
@@ -14,5 +27,20 @@ public class TaskController {
         task.setDescription("Create project structure");
         task.setTaskType(TaskType.WORK);
         return task;
+    }
+
+    @GetMapping("/log")
+    public BudgetEntry logSthg() {
+        try {
+            return restTemplate.exchange("http://localhost:9001/budget/1",
+                    HttpMethod.GET
+                    , null, new ParameterizedTypeReference<BudgetEntry>() {
+                    }).getBody();
+        } catch (Exception e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    e.getMessage()
+            );
+        }
     }
 }
