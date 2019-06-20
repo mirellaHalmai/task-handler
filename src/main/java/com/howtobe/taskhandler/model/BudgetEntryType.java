926f918db3e8431ea08e88cc2f67771ef404f18d
mirellaHalmai <halmaimirella@gmail.com>
2019-06-20 11:27:27 +0200

    code field for jpa

--- src/main/java/com/howtobe/taskhandler/model/BudgetEntryType.java
+++ src/main/java/com/howtobe/taskhandler/model/BudgetEntryType.java
@@ -1,6 +1,15 @@
 package com.howtobe.taskhandler.model;
 
+import lombok.Getter;
+
+@Getter
 public enum BudgetEntryType {
-    INCOME,
-    EXPENDITURE
+    INCOME (1),
+    EXPENDITURE (0);
+
+    private int code;
+
+    BudgetEntryType(int code) {
+        this.code = code;
+    }
 }
