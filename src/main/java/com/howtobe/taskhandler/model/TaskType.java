e30ce1c73fa0a09b497fbbd634e35833f1962ae7
mirellaHalmai <halmaimirella@gmail.com>
2019-06-20 10:18:35 +0200

    enum fields for name and code in db

--- src/main/java/com/howtobe/taskhandler/model/TaskType.java
+++ src/main/java/com/howtobe/taskhandler/model/TaskType.java
@@ -1,10 +1,21 @@
 package com.howtobe.taskhandler.model;
 
+import lombok.Getter;
+
+@Getter
 public enum TaskType {
 
-    BUDGET,
-    WORK,
-    FUN,
-    ADMINISTRATION,
-    OTHER
+    BUDGET("BUD", "Budget"),
+    WORK("WRK", "Work"),
+    FUN("FUN", "Fun"),
+    ADMINISTRATION("ADM", "Administration"),
+    OTHER("OTH", "Other");
+
+    private String code;
+    private String name;
+
+    TaskType(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
 }
