902ec93c5c22cd1aa7a2b01a7733ae367fd8744f
mirellaHalmai <halmaimirella@gmail.com>
2019-06-20 11:04:10 +0200

    enum field code for db

--- src/main/java/com/howtobe/taskhandler/model/BudgetEntryStatus.java
+++ src/main/java/com/howtobe/taskhandler/model/BudgetEntryStatus.java
@@ -1,6 +1,16 @@
 package com.howtobe.taskhandler.model;
 
+import lombok.Getter;
+
+@Getter
 public enum BudgetEntryStatus {
-    FUTURE,
-    DONE
+    TODO ("T"),
+    DONE_FOR_PERIOD ("DP"),
+    DONE ("D");
+
+    private String code;
+
+    BudgetEntryStatus(String code) {
+        this.code = code;
+    }
 }
